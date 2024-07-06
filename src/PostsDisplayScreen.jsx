278c3c1a606027fed52eeefd57ab5f68e30c6dd2
karunakar <karunakaryadav167@gmail.com>
2024-07-06 18:31:53 +0530

    Updated code

--- src/PostsDisplayScreen.jsx
+++ src/PostsDisplayScreen.jsx
@@ -1,11 +1,110 @@
 
+// import React, { useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { updatePost, deletePost } from './postsSlice.jsx';
+// import "./postdisplayscreen.css";
+
+
+
+// const PostsDisplay = () => {
+//   const posts = useSelector((state) => state.posts.posts);
+//   const dispatch = useDispatch();
+//   const [editingPostId, setEditingPostId] = useState(null);
+//   const [editedTitle, setEditedTitle] = useState('');
+//   const [editedContent, setEditedContent] = useState('');
+
+
+
+
+// const handleEditClick = (post) => {
+//     setEditingPostId(post.id);
+//     setEditedTitle(post.title);
+//     setEditedContent(post.content);
+//   };
+
+//   const handleUpdatePost = (e) => {
+//     e.preventDefault();
+//     dispatch(updatePost({ id: editingPostId, title: editedTitle, content: editedContent }));
+//     setEditingPostId(null);
+//     setEditedTitle('');
+//     setEditedContent('');
+//   };
+
+// const handleDeletePost = (a)=>{
+ 
+//   dispatch( deletePost(a))
+
+// }
+
+
+
+
+
+
+// return (
+//     <div  className="overal_container">
+          
+//             <h1>Posts</h1>
+           
+
+           
+//               {posts.map((post) => (
+//                 <div key={post.id}>
+//                   {editingPostId === post.id ? (
+//                                           <form onSubmit={handleUpdatePost}  className='post_display_form_container'>
+//                                                 <input
+//                                                   type="text"
+//                                                   value={editedTitle}
+//                                                   onChange={(e) => setEditedTitle(e.target.value)}
+//                                                 />
+//                                                 <textarea
+//                                                   value={editedContent}
+//                                                   onChange={(e) => setEditedContent(e.target.value)}
+//                                                 />
+//                                                 <button type="submit">Save</button>
+//                                           </form>
+//                   ) : (
+//                                               <div className="post_lists_container">
+//                                                 <h2>{post.title}</h2>
+//                                                 <p>{post.content}</p>
+//                                                 <button onClick={() => handleEditClick(post)}>Edit</button>
+//                                                 <button onClick={()=>handleDeletePost({id:post.id})}> Delete</button>
+//                                               </div>
+//                   )}
+
+
+                
+
+
+//                 </div>
+//               ))}
+
+
+
+              
+//             <Link to="/">Create New Post</Link>
+   
+   
+//      </div>
+  
+//     );
+
+//   };
+
+// export default PostsDisplay;
+
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { updatePost, deletePost } from './postsSlice.jsx';
 import "./postdisplayscreen.css";
-
-
 
 const PostsDisplay = () => {
   const posts = useSelector((state) => state.posts.posts);
@@ -14,10 +113,7 @@ const PostsDisplay = () => {
   const [editedTitle, setEditedTitle] = useState('');
   const [editedContent, setEditedContent] = useState('');
 
-
-
-
-const handleEditClick = (post) => {
+  const handleEditClick = (post) => {
     setEditingPostId(post.id);
     setEditedTitle(post.title);
     setEditedContent(post.content);
@@ -31,64 +127,47 @@ const handleEditClick = (post) => {
     setEditedContent('');
   };
 
-const handleDeletePost = (a)=>{
- 
-  dispatch( deletePost(a))
+  const handleDeletePost = (a) => {
+    dispatch(deletePost(a))
+  }
 
-}
-
-
-
-
-
-
-return (
-    <div  className="overal_container">
-          
-            <h1>Posts</h1>
-           
-              {posts.map((post) => (
-                <div key={post.id}>
-                  {editingPostId === post.id ? (
-                                          <form onSubmit={handleUpdatePost}  className='post_display_form_container'>
-                                                <input
-                                                  type="text"
-                                                  value={editedTitle}
-                                                  onChange={(e) => setEditedTitle(e.target.value)}
-                                                />
-                                                <textarea
-                                                  value={editedContent}
-                                                  onChange={(e) => setEditedContent(e.target.value)}
-                                                />
-                                                <button type="submit">Save</button>
-                                          </form>
-                  ) : (
-                                              <div className="post_lists_container">
-                                                <h2>{post.title}</h2>
-                                                <p>{post.content}</p>
-                                                <button onClick={() => handleEditClick(post)}>Edit</button>
-                                                <button onClick={()=>handleDeletePost({id:post.id})}> Delete</button>
-                                              </div>
-                  )}
-                </div>
-              ))}
-            <Link to="/">Create New Post</Link>
-   
-   
-     </div>
-  
-    );
-
-  };
+  return (
+    <div className="overal_container">
+      <h1>Posts</h1>
+      {posts.length === 0? (
+        <h5>No posts created yet. Create a new post by clicking the link below.</h5>
+      ) : (
+        posts.map((post) => (
+          <div key={post.id}>
+            {editingPostId === post.id? (
+              <form onSubmit={handleUpdatePost} className='post_display_form_container'>
+                <input
+                  type="text"
+                  value={editedTitle}
+                  onChange={(e) => setEditedTitle(e.target.value)}
+                />
+                <textarea
+                  value={editedContent}
+                  onChange={(e) => setEditedContent(e.target.value)}
+                />
+                <button type="submit">Save</button>
+              </form>
+            ) : (
+              <div className="post_lists_container">
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+                <button onClick={() => handleEditClick(post)}>Edit</button>
+                <button onClick={() => handleDeletePost({ id: post.id })}> Delete</button>
+              </div>
+            )}
+          </div>
+        ))
+      )}
+      <Link to="/">Create New Post</Link>
+    </div>
+  );
+};
 
 export default PostsDisplay;
-
-
-
-
-
-
-
-
 
 
